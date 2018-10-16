a0e0f3fcec7ee2857be46ebf44cd67beb3ed361f
Emily Asch <e.asch@me.com>
2018-10-16 22:59:09 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,9 +1,17 @@
 # Write your code here.
 def dictionary 
   {
-    "hello" => "hi",
-    "to"
-  }
+    "hello" => "hi", 
+    "to" => "2",
+    "too" => "2",
+    "two" => "2",
+    "for" => "4", 
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+     "at" => "@",
+    "and" => "&"
+  }  
 end
 
 def word_substituter
